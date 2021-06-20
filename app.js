639ee554a7647a632a166e57e8be58b0680fbfcd
netsus <57648890+netsus@users.noreply.github.com>
2021-06-20 19:17:15 +0900

    #2.0 Canvas Events
    1.app.js
      1) 마우스 관련 이벤트 처리
      ** addEventListener("mousemove", onMouseMove) => mousemove이벤트가 발생하면
    onMouseMove함수에서 event를 받아옴 -> json형태의 event 안에 있는 정보를 사용할 수 있다.
    --> onMouseMove 함수:
    function onMouseMove(event){
        const x = event.offsetX;
        const y = event.offsetY;
    }
    
      2) 마우스 관련 이벤트들
      mousemove, mousedown, mouseup, mouseleave 이벤트 각각에 대해 addEventListener 줄 수 있다.
     -->각각, 마우스 움직임, 클릭, 클릭 떼기, 지정된 영역에서 마우스가 떠나는 이벤트를 의미

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const canvas = document.getElementById("jsCanvas");
 
 
-function onMouseMove(event){
-    console.log(event)
+let painting = false;
+
+function stopPainting(){
+    painting = true;
 }
 
+function onMouseMove(event){
+    const x = event.offsetX;
+    const y = event.offsetY;
+    // console.log(x,y);
+}
+
+function onMouseDown(event){
+    painting = true;
+}
+
+function onMouseUp(event){
+    stopPainting();
+}
 
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove)
-}
+    canvas.addEventListener("mousedown", onMouseDown)
+    canvas.addEventListener("mousedown", onMouseUp)
+    canvas.addEventListener("mousedown", stopPainting)
+} 
