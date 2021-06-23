ed1debc14a3af535102c41b83719a3e6ff61e94b
netsus <57648890+netsus@users.noreply.github.com>
2021-06-24 00:29:13 +0900

    #2.3 Changing Color
    onMouseMove함수
    --> 선을 만들고 그 선들에 색을 채우는 역할
    
    1. HTML에서 색깔 가져오기
    const colors = document.getElementsByClassName("jsColor")
    
    Array.from(colors);   --> colors를 array로 만들기

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor")
 
 canvas.width = 750;
 canvas.height = 750;
@@ -44,3 +45,5 @@ if(canvas){
     canvas.addEventListener("mouseup", stopPainting)
     canvas.addEventListener("mouseleave", stopPainting)
 } 
+
+console.log(Array.from(colors));
